f4c256840fff0978866e332cbb1e8c1fd822f3d7
voldig@gmail.com <1705217@rgu.ac.uk>
2019-03-06 12:40:58 +0000

    tested on codio finished

---
+++ lab_10/getresources.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	//all code go in here
+	$('#retrieve-resources').click(function() {
+		var displayResources = $('#display-resources');
+		displayResources.text('Loading data from JSON source...');
+
+		//uses JQuery AJAX functionn to retrieve the JSON file. 
+		$.ajax({
+			type:"GET",
+			url: "resources.json",
+			success: function(result) {
+				//build a table from resources
+				var output = "<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";	
+				for(var i in result) {
+					output += "<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
+				}
+				output += "</tbody></table>"; //closing table
+
+				displayResources.html(output); 
+				$("table").addClass("table"); //adds class table for styling
+			}});
+		});
+});
