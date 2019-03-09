d422cd8953b1eec6765bb135248b25dd6d97805b
voldig@gmail.com <1705217@rgu.ac.uk>
2019-03-09 16:06:17 +0000

    finished lab 10

--- lab_10/roverparser.js
+++ lab_10/roverparser.js
@@ -1,10 +1,3 @@
-/**
- * @Author: John Isaacs <john>
- * @Date:   23-Feb-182018
- * @Filename: getresources.js
- * @Last modified by:   john
- * @Last modified time: 23-Feb-182018
- */
  $(document).ready(function () {
 
   $('#retrieve-data').click(function () {
@@ -20,12 +13,14 @@
   console.log(result.photos);
   var photos = result.photos //this data contains an array called photos
 
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th></thead><tbody>";
+  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Earth Date</th><th>Lounch data</th></thead><tbody>";
   for (var i in photos)
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
-    + photos[i].camera.full_name + "</td><td>"
-    + photos[i].img_src + "</td></tr>";
+    + photos[i].camera.full_name + "</td><td><img src='"
+    + photos[i].img_src + "'></td><td>"
+    + photos[i].earth_date + "</td><td>"
+    + photos[i].launch_date + "</td></tr>";
   }
   output+="</tbody></table>";
 
