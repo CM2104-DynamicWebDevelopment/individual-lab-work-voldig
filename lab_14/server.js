483c1c06a88a3e3bcc612ae08c51bf70a18fe241
voldig <voldig@gmail.com>
2019-03-22 15:25:53 +0000

    john solution

--- lab_14/server.js
+++ lab_14/server.js
@@ -1,32 +1,43 @@
+/**
+ * @Author: John Isaacs <john>
+ * @Date:   11-Mar-182018
+ * @Filename: server.js
+ * @Last modified by:   john
+ * @Last modified time: 12-Mar-182018
+ */
 var express = require('express');
-	var app = express();
-	var Twitter = require('twitter');
-	var params = {screen_name: 'nodejs'};
+var app = express();
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'vPuzIzncL5b5HyyUMp99kbMwv',
+  consumer_secret: 'yKbWuC6TupPNzIJP0NnNXVxLn1D8BDprijakencUG3bAGMize5',
+  access_token_key: '14812487-PNs6GC8wpgeAG2W7szRt4ZvpTA7lYBes6mORd9KDT',
+  access_token_secret: 'Tjy5TrQ5c5YVLA6q2cb7LeoDty7bHIg0HmbDELgMwMCHG'
+});
 
 
-	var client = new Twitter({
-		consumer_key: "U5b0iyQZEdTj5aQ3NzVeh8jxS",
-		consumer_secret: "Jtbk8gC5ra94tkdhQTdbVexx35nDL1EjHuvl4w2LGbknE2jRR4",
-		access_token_key: "1109036022015381505-yhTpfIGLNUUcUkyYSKuo8vkH37etfK",
-		access_token_secret: "mlxj3ZPGUlYEMtH3aMXyMvcDuZdreKd5BPyAzgkmjzre8"
-	});
+app.use(express.static('public'))
 
 
 
-	app.use(express.static('public')); //all static html stuff is in folder public
+app.get('/tweetsjson', function(req, res) {
+  var params = {
+    screen_name: 'nodejs'
+  };
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      var json = [];
+      for (var i = 0; i < tweets.length; i++) {
+        json.push({
+          name: tweets[i].user.name,
+          text: tweets[i].text
+        });
+      }
+      res.send(JSON.stringify(json));
+    }
+  });
+});
 
-	app.get('/', function(req, res){
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
-			if(!error) {
-				 var output ="";
-				 for(var t =0; t < tweets.length; t++){
-				 	output +="<div>";
-					output +="<h2>"+ tweets[t].user.screen_name +"<h2>";
-					output +="<p>"+ tweets[t].text +"</p>";
-					output +="</div>";
-				}
-			}
-		});
-	});
 
-	app.listen(8080);
+app.listen(8080);
