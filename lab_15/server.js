ddd085602c457cb14116a16d898034225720bb4c
voldig <voldig@gmail.com>
2019-03-25 14:32:25 +0000

    begin lab 17

--- lab_15/server.js
+++ lab_15/server.js
@@ -45,5 +45,5 @@ app.post('/quotes', function (req, res) {
 
 
 
-	
+
 });
---
+++ lab_17/views/server.js
@@ -0,0 +1,21 @@
+//load express as app
+var express = require('express');
+var app = express();
+
+//set the view engine to EJS
+app.set('view engine', 'ejs');
+
+//use res.render to laoad up an ejs view file
+
+// indes page
+app.get('/', function(req, res) {
+  res.render('page/index');
+});
+
+//about pages
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the port which it is using');
