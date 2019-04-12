1e245059a10d3e504273940bb7950e0c5645d6be
voldig <voldig@gmail.com>
2019-04-12 11:24:23 +0100

    added ability to add new points

---
+++ groupWork/final/database/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const PointSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    latitude: Number,
+    longitude: Number
+});
+
+const Point = mongoose.model('Point', PointSchema);
+
+module.exports = Point;
--- groupWork/final/server.js
+++ groupWork/final/server.js
@@ -1,7 +1,14 @@
 // server.js
 // load the things we need
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
+
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
@@ -12,6 +19,9 @@ app.get('/', function(req, res) {
 // about page
 app.get('/about', function(req, res) {
     res.render('pages/about');
+});
+app.get('/points/new', function(req, res) {
+    res.render('addPoint')
 });
 // this one line is important, without it css and js can't be fetch - https://stackoverflow.com/questions/26019456/external-js-in-an-ejs-template
 app.use(express.static(__dirname));
