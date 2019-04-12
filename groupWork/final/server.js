670e72b1f6c1a177b3478ac8c687dbb91b7e598f
voldig <voldig@gmail.com>
2019-04-12 10:45:08 +0100

    try

--- groupWork/final/server.js
+++ groupWork/final/server.js
@@ -14,6 +14,7 @@ app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 //without that css was not fetched. Found that on https://stackoverflow.com/questions/49595938/error-comes-when-try-to-link-add-css-to-ejs-file
-app.use('/css', express.static('css'));
+// app.use('/css', express.static('css'));
+app.use(express.static(__dirname));
 app.listen(8080);
 console.log('8080 is the magic port');
