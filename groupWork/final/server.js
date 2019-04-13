945dceb10698c9ef7a337f01d851062757467c6f
voldig <voldig@gmail.com>
2019-04-13 10:47:52 +0100

    Added authentication

--- groupWork/final/server.js
+++ groupWork/final/server.js
@@ -4,14 +4,29 @@ const express = require('express');
 const mongoose = require('mongoose');//this is module for "talking" to mongoDB. It makes Mongo bahave in more structured way
 const bodyParser = require('body-parser');
 const Point = require('./database/models/Point'); //thats where the point model is(basically what data)
-
+const bcrypt = require('bcrypt') //for encryption of the passwords
+const expressSession = require('express-session');//persistent logins
+const connectMongo = require('connect-mongo');
 
 const app = express();
+
+app.use(expressSession({
+    secret: 'secret'
+}));
+
 
 mongoose.connect('mongodb://localhost:27017/eclipsim', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
 
+const mongoStore = connectMongo(expressSession);
+
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
 // from the labs - ejs templating engine
 app.set('view engine', 'ejs');
 
@@ -38,7 +53,11 @@ app.get('/about', function(req, res) {
 
 
 app.get('/points/new', function(req, res) {
-    res.render('pages/addPoint.ejs')
+  //checking if user is logged in, if not redirect to login page
+  if (req.session.userId) {
+      return res.render('pages/addPoint');
+  }
+    res.redirect('/auth/login');
 });
 app.post('/points/store', function(req, res) {
     //console.log(req.body)
@@ -47,9 +66,49 @@ app.post('/points/store', function(req, res) {
     })
 });
 
+//registration routes
+app.get("/auth/register", function(req, res){
+  res.render('pages/register');
+});
 
+app.post("/users/register", function(req, res){
+  User.create(req.body, function(error, user) {
+      if (error) {
+          return res.redirect('/auth/register')
+      }
+      res.redirect('/')
+  })
+});
+//login routers
+app.get('/auth/login', function(req, res){
+  res.render('pages/login');
+});
 
-
+app.post('/users/login', function(req, res){
+  const {
+        email,
+        password
+    } = req.body;
+    // try to find the user
+    User.findOne({
+        email
+    }, (error, user) => {
+        if (user) {
+            // compare passwords.
+            bcrypt.compare(password, user.password, (error, same) => {
+                if (same) {
+                    // store user session.
+                    req.session.userId = user._id
+                    res.redirect('/')
+                } else {
+                    res.redirect('/auth/login')
+                }
+            })
+        } else {
+            return res.redirect('/auth/login')
+        }
+    })
+});
 
 
 
