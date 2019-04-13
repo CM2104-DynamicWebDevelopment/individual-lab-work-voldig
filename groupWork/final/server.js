9bbe76848b5af40bf947a5cbb2baf1db9029ba2a
voldig <voldig@gmail.com>
2019-04-14 00:21:23 +0100

    dfdafda

--- groupWork/final/server.js
+++ groupWork/final/server.js
@@ -101,7 +101,7 @@ app.post('/users/login', function(req, res){
             bcrypt.compare(password, user.password, (error, same) => {
                 if (same) {
                     // store user session.
-                    req.session.userId = user._id
+                    // req.session.userId = user._id
                     res.redirect('/')
                 } else {
                     res.redirect('/auth/login')
