c9a80e6cd73e7901b818e5c66913dbe2aab783cf
voldig <voldig@gmail.com>
2019-04-13 10:07:04 +0100

    dfadf

--- groupWork/final/middleware/storePoint.js
+++ groupWork/final/middleware/storePoint.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
     if (!req.body.title || !req.body.description || !req.body.latitude || !req.body.longitude) {
-        alert("all fields are required!");
         return res.redirect('/points/new')
     }
     next()
